2517d6ae58ca31f9a536a6fa1a48f0353d53506e
Brandon T <btai@bayviewglen.ca>
2024-01-08 14:05:36 -0500

    dist update again

--- src/components/description.vue
+++ src/components/description.vue
@@ -6,7 +6,7 @@
 				<div class="col-md-12">
 					<p class="hi">Hi, my name is</p>
 					<h1 class="name">Brandon Tai.</h1>
-					<h1 class="name-subtitle">I build applications for the Web.</h1>
+					<h1 class="name-subtitle">I build applications for the web.</h1>
 					<p class="name-desc">I'm a full stack developer in Grade 12 located in Toronto, Canada. I am passionate about creating and developing applications that push the boundaries of what's possible.</p>
 				</div>
 			</div>
