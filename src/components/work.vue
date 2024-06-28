09345205c1173393579540f657130f38642e81f6
Brandon T <btai@bayviewglen.ca>
2024-06-28 19:01:37 -0400

    fix job component

--- src/components/work.vue
+++ src/components/work.vue
@@ -1,47 +1,90 @@
 <template>
-  <div class="home">
-		<!-- work -->
-		<section class="container work" id="experience">
-			<div class="row">
-				<div class="col-md-12">
-					<h3><span>02. </span>Where I've Worked</h3>
-				</div>
-				<div class="work-holder row">
-					<div class="col-md-2 work-tab">
-						<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+	<div class="home">
+	  <!-- work -->
+	  <section class="container work" id="experience">
+		<div class="columns is-multiline">
+		  <div class="column is-12">
+			<h3><span>02. </span>Where I've Worked</h3>
+		  </div>
+		  <div class="column is-12">
+			<div class="tabs is-centered is-boxed">
+			  <ul>
+			
+			  </ul>
+			</div>
+		  </div>
+		  <div class="column is-12">
+			<div class="tab-content" id="tab-content">
+			  <div class="content tab-1">
+				<h6 style="color: var(--white)">
+				  Management Engineer Intern
+				  <span style="color: var(--green)">
+					<a href="https://www.qvella.com/canada/">@ Qvella</a>
+				  </span>
+				</h6>
+				<p>July 2023 - August 2023</p>
+				<ul class="fa-ul pl-0 ml-3">
+				  <li>
+					<span class="fa-li"><i class="fas fa-caret-right"></i></span>
+					Streamlined supplier data, improved procurement efficiency via a database, optimized parts purchasing, minimized excess stock through the warehouse.
+				  </li>
+				</ul>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </section>
+	</div>
+  </template>
+  
+  <script>
+//   export default {
+// 	mounted() {
+// 	  // Add any JavaScript/jQuery needed for tab functionrAll('.tabs ul li').forEach(tab => {
+// 		tab.addEventListener('click', () => {
+// 		  document.querySelectorAll('.tabs ul li').forEach(tab => {
+// 			tab.classList.remove('is-active');
+// 		  });
+// 		  tab.classList.add('is-active');
+// 		  const tabContent = document.getElementById('tab-content');
+// 		  tabContent.querySelectorAll('.content').forEach(content => {
+// 			content.style.display = 'none';
+// 		  });
+// 		  tabContent.querySelector('.' + tab.getAttribute('data-tab')).style.display = 'block';
+// 		});
+// 	  }
+// 	}
+
+  </script>
+  
+  <style scoped>
+  /* Add any additional styles for responsiveness */
+  .content {
+	display: none;
+  }
+  .content.tab-1 {
+	display: block;
+  }
+  </style>
+  
+
+  <!-- 		  <div class=" row mobile-work-holder d-sm-block d-md-none">
+					  <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+						  <li class="nav-item">
 							<a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Qvella</a>
+						</li>
+					
+					  </ul>
+					  <div class="tab-content" id="pills-tabContent">
+						<div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"><h6 style="color: var(--white)">Management Engineer Intern  <span style="color: var(--green)"><a href="https://www.qvella.com/canada/">@ Qvella</a></span> </h6><p>July 2023 - August 2023</p>
+							<ul class="fa-ul pl-0 ml-3">
+								  <li><span class="fa-li"><i class="fas fa-caret-right"></i></span>Streamlined supplier data, improved procurement efficiency via a database, optimized parts purchasing, minimized excess stock through the warehouse. </li>
+								  <!-- <li><span class="fa-li"><i class="fas fa-caret-right"></i></span>Brainstormed ideas for my group during the internship</li> -->
+								</ul>
+							</div>
+						  
+							
 						</div>
 					</div>
-					<div class="col-md-10">
-						<div class="tab-content pl-2" id="v-pills-tabContent">
-							<div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"><h6 style="color: var(--white)">Management Engineer Intern  <span style="color: var(--green)"><a href="https://www.qvella.com/canada/">@ Qvella</a></span> </h6><p>July 2023 - August 2023</p>
-								<ul class="fa-ul pl-0 ml-3">
-									<li><span class="fa-li"><i class="fas fa-caret-right"></i></span>Streamlined supplier data, improved procurement efficiency via a database, optimized parts purchasing, minimized excess stock through the warehouse. </li>
-								</ul>
-							</div>
-						
-							<!-- <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
-								<ul class="fa-ul pl-0 ml-3">
-									<li><span class="fa-li"><i class="fas fa-caret-right"></i></span>HTML & (S)CSS</li>
-									<li><span class="fa-li"><i class="fas fa-caret-right"></i></span>JavaScript (ES6+)</li>
-									<li><span class="fa-li"><i class="fas fa-caret-right"></i></span>Bootstrap</li>
-									<li>M<span class="fa-li"><i class="fas fa-caret-right"></i></span>aterialize CSS</li>
-								</ul>
-							</div>
-							<div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
-								<ul class="fa-ul pl-0 ml-3">
-									<li><span class="fa-li"><i class="fas fa-caret-right"></i></span>HTML & (S)CSS</li>
-									<li><span class="fa-li"><i class="fas fa-caret-right"></i></span>JavaScript (ES6+)</li>
-									<li><span class="fa-li"><i class="fas fa-caret-right"></i></span>Bootstrap</li>
-									<li>M<span class="fa-li"><i class="fas fa-caret-right"></i></span>aterialize CSS</li>
-								</ul>
-							</div> -->
-						</div>
-					</div>					
-				</div>
-				<!-- Mobile work holder -->
-				
-			</div>
-		</section>
-  </div>
-</template>
+					
+					 -->
